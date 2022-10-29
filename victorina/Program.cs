1fbda93a5e3b1b2ca388abc17a941aca5eae8db3
Егор <@KITTLE>
2022-10-29 18:20:13 +0300

    Add project files.

---
+++ victorina/Program.cs
@@ -0,0 +1,91 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var arrUsers = new Users[]
+        {
+            new Users("admin","admin",33432004),
+            new Users("egor","password",32321999)
+        };
+
+    Start:
+        Console.WriteLine("Чтобы авторизироваться введите 1, Чтобы зарегистрироваться введите 2");
+        var input = Console.ReadLine();
+
+
+
+        bool successfull = false;
+        while (!successfull)
+        {
+
+            if (input == "1")
+            {
+                Console.WriteLine("Напишите имя пользователя::");
+                var username = Console.ReadLine();
+                Console.WriteLine("Введите свой пароль:");
+                var password = Console.ReadLine();
+
+
+                foreach (Users user in arrUsers)
+                {
+                    if (username == user.username && password == user.password)
+                    {
+                        Console.WriteLine("Вы успешно вошли в систему");
+                        Console.ReadLine();
+                        successfull = true;
+                        break;
+                    }
+                }
+
+                if (!successfull)
+                {
+                    Console.WriteLine("Ваш логин или пароль неверны, попробуйте еще раз!");
+                }
+
+            }
+
+            else if (input == "2")
+            {
+
+                Console.WriteLine("Введите имя пользователя:");
+                var username = Console.ReadLine();
+
+                Console.WriteLine("Введите свой пароль:");
+                var password = Console.ReadLine();
+
+                Console.WriteLine("Введите свою дату рождения:");
+                int id = int.Parse(Console.ReadLine());
+
+
+                Array.Resize(ref arrUsers, arrUsers.Length + 1);
+                arrUsers[arrUsers.Length - 1] = new Users(username, password, id);
+                successfull = true;
+                goto Start;
+
+            }
+            else
+            {
+                Console.WriteLine("Попробуйте еще раз!");
+                break;
+
+
+            }
+
+        }
+
+    }
+}
+
+public class Users
+{
+    public string username;
+    public string password;
+    private int id;
+
+    public Users(string username, string password, int id)
+    {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+    }
+}
---
+++ victorina/User.cs
@@ -0,0 +1,8 @@
+﻿namespace WindowsFormsApp2
+{
+    public class User
+    {
+        public object Login { get; internal set; }
+        public object Password { get; internal set; }
+    }
+}
